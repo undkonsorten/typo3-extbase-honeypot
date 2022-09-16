fb3c3bad2348a6b8ea826e23f9e2f1bb58458f9d
Felix Althaus <felix.althaus@undkonsorten.com>
2022-09-16 15:46:57 +0200

    [TASK] Extend compatibility to TYPO3 11.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,10 +12,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '0.0.2',
+    'version' => '0.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5 - 10.4.99',
+            'typo3' => '9.5 - 11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
